f0a18c632ac486b49feb45ff636adcf5136b5374
Kilian <kilian.breton.softk@gmail.com>
2019-02-28 16:39:28 +0100

    vendor

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 //src/Controller/LuckyController.php
 namespace App\Controller;
 
+use App\Entity\Visiteur;
 use Symfony\Component\HttpFoundation\Response;
 
 class MainController
@@ -10,10 +11,23 @@ class MainController
 
     public function index()
     {
-      
-        return new Response(
-           
-            '<html><body><h1>hello world !</h1></body></html>'
+        $entityManager = $this->getDoctrine()->getManager();
+        $vis = new Visiteur();
+        $vis->setVisMatricule('z0z0z');
+        $vis->setVisLogin('z0z0z');
+        $vis->setVisNom('z0z0z');
+        $vis->setVisPrenom('z0z0z');
+        $vis->setVisAdresse('z0z0z');
+        $vis->setVisCp('z0z0z');
+        $vis->setVisVille('z0z0z');
+ //       $vis->setVisDateembauche(new DateTimeImmutable());
+        $vis->setVisPassword('z0z0z');
+        $vis->setVisSalt('z0z0z');
+        $vis->setSecCode('z');
+        $vis->setLabCode('z');
+        $entityManager->persist($product);
+
+        return new Response('<html><body><h1>hello world !</h1></body></html>'
         );
     }
 }
