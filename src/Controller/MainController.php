f187c74cde5d1cde103f9fc4db06412fc26c0cb0
Kilian <kilian.breton.softk@gmail.com>
2019-03-05 11:26:59 +0100

    jkdsnf

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,23 +11,8 @@ class MainController
 
     public function index()
     {
-        $entityManager = $this->getDoctrine()->getManager();
-        $vis = new Visiteur();
-        $vis->setVisMatricule('z0z0z');
-        $vis->setVisLogin('z0z0z');
-        $vis->setVisNom('z0z0z');
-        $vis->setVisPrenom('z0z0z');
-        $vis->setVisAdresse('z0z0z');
-        $vis->setVisCp('z0z0z');
-        $vis->setVisVille('z0z0z');
-        $vis->setVisDateembauche(new DateTimeImmutable());
-        $vis->setVisPassword('z0z0z');
-        $vis->setVisSalt('z0z0z');
-        $vis->setSecCode('z');
-        $vis->setLabCode('z');
-        $entityManager->persist($product);
-
-        return new Response('<html><body><h1>hello world !</h1></body></html>'
-        );
+     
+     
+        return new Response('<html><body><h1>hello world !</h1></body></html>');
     }
 }
